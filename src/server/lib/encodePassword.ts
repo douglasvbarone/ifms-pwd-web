8ce77400d6eb7bd3f8a02fba7fbd351028880c75
Douglas Barone <douglasvbarone@gmail.com>
2023-12-15 17:25:18 -0400

    Refactor encodePassword function to improve password encoding

--- src/server/lib/encodePassword.ts
+++ src/server/lib/encodePassword.ts
@@ -1,11 +1,12 @@
 export function encodePassword(password: string): string {
   let newPassword = ''
-  password = '"' + password + '"'
+
   for (let i = 0; i < password.length; i++) {
     newPassword += String.fromCharCode(
       password.charCodeAt(i) & 0xff,
       (password.charCodeAt(i) >>> 8) & 0xff
     )
   }
-  return newPassword
+
+  return `"${newPassword}"`
 }
