858c0ceb486faaf51a48c4de21b7a3c4344e0a34
Douglas Barone <douglasvbarone@gmail.com>
2023-12-19 08:42:00 -0400

    Update rate limit settings in server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,8 +4,8 @@ import cors from 'cors'
 import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 60 * 1000 * 15, // 15 minutes
+  max: 50 // limit each IP to 50 requests per windowMs
 })
 
 const server = express()
