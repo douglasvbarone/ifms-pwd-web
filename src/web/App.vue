bfd25afaf173d5fcef98410de5d3bd7a3cd6cf16
Douglas Barone <douglasvbarone@gmail.com>
2024-12-05 12:57:36 -0400

    Enhance footer with version link and icon in App.vue

--- src/web/App.vue
+++ src/web/App.vue
@@ -3,7 +3,15 @@
     <v-main>
       <router-view />
       <v-footer app class="text-grey">
-        <span class="mr-2">v{{ version }}</span>
+        <span class="mr-2">
+          <v-btn
+            variant="text"
+            href="https://git.pp.ifms.edu.br/1839811/ifms-pwd-web"
+            target="_blank"
+          >
+            <v-icon class="mr-2">mdi-git</v-icon> {{ version }}
+          </v-btn>
+        </span>
         <v-spacer />
         <span class="mr-2">SERTI-PP</span>
       </v-footer>
